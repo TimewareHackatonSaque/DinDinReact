ade921a4014c0a4a2cac5bf03b61e02026395b6c
FelipeStemp <felipestempkowski17@gmail.com>
2024-09-09 23:42:35 -0300

    Atualizando, Tela de Opções finalizada

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import { BrowserRouter as Router } from 'react-router-dom';
 import Home from './Pages/Home/home';
 
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Home />
-    </Router>
+    <Home/>
   )
 }
 
--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Footer from '../../components/footer/footer';
-import Header from '../../components/header/header';  // Importando o Header
+import Header from '../../components/header/header'; // Importando o Header
 
 const Home = () => {
   return (
