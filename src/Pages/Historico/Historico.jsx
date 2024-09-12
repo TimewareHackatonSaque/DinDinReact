9593ea059381dcd5c65850f028eec72b738c1afc
FelipeStemp <felipestempkowski17@gmail.com>
2024-09-12 05:11:45 -0300

    Atualização, conexão com api

--- src/Pages/Historico/Historico.jsx
+++ src/Pages/Historico/Historico.jsx
@@ -1,71 +1,106 @@
+import { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
 import Footer from "../../components/footer/footer";
 import IconLeft from '../../components/IconLeft/left';
-import { Link } from "react-router-dom";
-import Espaco from "../../components/space/space"
-import * as C from './styled';
 import LineGradiente from '../../components/line/line';
-
-
+import Espaco from "../../components/space/space";
+import * as C from './styled';
 
 function Historico() {
+  const [id, setId] = useState('');
+  const [data, setData] = useState([]); // Adicione o estado data
+  const [error, setError] = useState('');
+  
+  useEffect(() => {
+    // Recupera o userId do localStorage e define no estado
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      const parsedUser = JSON.parse(storedUser);
+      if (parsedUser.user) {
+        setId(parsedUser.user.id);
+      }
+    }
+  }, []);
+  
+  useEffect(() => {
+    // Faz a requisição apenas se o id estiver disponível
+    const fetchSimulationDetails = async () => {
+      if (id) {
+        try {
+          const url = `http://localhost:3001/historico/user/${id}`;
+  
+          const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          });
+  
+          if (!response.ok) {
+            throw new Error('Erro na requisição');
+          }
+  
+          const result = await response.json();
+          setData(result); // Atualize o estado data
+  
+        } catch (err) {
+          console.error('Erro ao consultar ofertas:', err);
+          setError('Ocorreu um erro ao consultar as ofertas.');
+        }
+      }
+    };
+  
+    fetchSimulationDetails();
+  }, [id]); 
 
-    return(
+  return (
+    <div>
+      <div>
+        <Link to="/home">  {/* Este link irá redirecionar para a home */}
+          <IconLeft margin="29px 0px 0px 32px" fontSize="24px" height="24px" />
+        </Link>
+          
+        <Espaco height="20px"></Espaco>
 
-        <div>
+        <C.TituloLogin>Histórico <br/> de cotação</C.TituloLogin>
+      </div>
 
-            <div>
-                <Link to="/home">  {/* Este link irá redirecionar para a home */}
-                <IconLeft margin="29px 0px 0px 32px" fontSize="24px" height="24px" />
-                </Link>
-                    
-                    <Espaco height="20px"></Espaco>
+      <Espaco height="20px"/>
 
-                    <C.TituloLogin>Histórico <br/>
-                     de cotação
-                    </C.TituloLogin>
+      <C.DivLine>
+        <LineGradiente
+          width="80vw"
+          height="2px"
+          backgroudColor="linear-gradient(270deg, #048F44 0%, #F69F00 100%)"
+        />
+      </C.DivLine>
 
-            </div>
+      <div>
+        <C.Card>
+            {error && <p>{error}</p>}
+            {data.length > 0 ? (
+            data.map((item, index) => (
+                <div key={index}>
+                <C.CardHeader>
+                    <C.Instituicao>Cotação</C.Instituicao>
+                </C.CardHeader>
+                <C.CardBody>
+                    <C.ValorTotal>R$ {item.loanAmount}</C.ValorTotal>
+                    <C.Parcelas>{item.numberOfInstallments}x de R$ {item.installment}</C.Parcelas>
+                </C.CardBody>
+                </div>
+            ))
+            ) : (
+            <p>Nenhuma simulação encontrada.</p>
+            )}
+        </C.Card>
+      </div>
 
-            <Espaco height="20px"/>
+      <Espaco height="20px"/>
 
-            <C.DivLine>
-                <LineGradiente
-                    width="80vw"
-                    height="2px"
-                    backgroudColor="linear-gradient(270deg, #048F44 0%, #F69F00 100%)"
-                />
-            </C.DivLine>
-
-            <div>
-                <C.Card>
-                    <C.CardHeader>
-                        <C.Instituicao>Banco XYZ</C.Instituicao>
-                    </C.CardHeader>
-                    <C.CardBody>
-                        <C.ValorTotal>R$ 10.000,00</C.ValorTotal>
-                        <C.Parcelas>12x de R$ 833,33</C.Parcelas>
-                    </C.CardBody>
-                </C.Card>
-
-                <C.Card>
-                    <C.CardHeader>
-                        <C.Instituicao>Banco ABC</C.Instituicao>
-                    </C.CardHeader>
-                    <C.CardBody>
-                        <C.ValorTotal>R$ 5.000,00</C.ValorTotal>
-                        <C.Parcelas>6x de R$ 833,33</C.Parcelas>
-                    </C.CardBody>
-                </C.Card>
-
-                {/* Adicione mais cards conforme necessário */}
-            </div>
-
-            <Espaco height="20px"/>
-
-            <Footer />
-       </div>
-    );
-
+      <Footer />
+    </div>
+  );
 }
 
 export default Historico;
